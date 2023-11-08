2b541ccc7ee8fe8fb5d2b17ebbae4a12c4ac9d89
Ashraf <150205980+AshrafMohammed22@users.noreply.github.com>
2023-11-08 03:55:37 +0200

    Create FindTheSummation.cpp

---
+++ FindTheSummation.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int arr1[5]={1,2,3,4,5};
+int sum =0;
+for (int i =0; i<5;i++)
+{
+   sum=sum+arr1[i];
+}
+    cout<<"the sum of elelment of the array equal :"<<sum<<endl;
+    return 0;
+}
